18e3d82ac6c4cbf8d43cb6d51362fd197cf7de6c
DeveloperToby <phantanlong121102@gmail.com>
2026-06-21 13:56:48 +0700

    use tab = 2

--- templates/cp.cpp
+++ templates/cp.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while(t--) {
-        
-    }
-
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--) {
+    
+  }
+  return 0;
 }
