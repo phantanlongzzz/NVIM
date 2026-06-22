c96c617d24db97ce03714a583dfc5c52169ccfc6
DeveloperToby <phantanlong121102@gmail.com>
2026-06-22 23:54:10 +0700

    buffnewfile template cpp

--- init.lua
+++ init.lua
@@ -26,27 +26,28 @@ vim.opt.signcolumn = "no"
 vim.opt.number = true           
 vim.opt.relativenumber = true     
 
-vim.api.nvim_create_autocmd("BufNewFile", {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
   pattern = "*.cpp",
   callback = function()
-    local lines = {
-      "#include <bits/stdc++.h>",
-      "using namespace std;",
-      "",
-      "int main() {",
-      "  ios::sync_with_stdio(false);",
-      "  cin.tie(nullptr);",
-      "",
-      "  return 0;",
-      "}",
-    }
+    if vim.api.nvim_buf_line_count(0) == 1 and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] == "" then
+      local lines = {
+        "#include <bits/stdc++.h>",
+        "using namespace std;",
+        "",
+        "int main() {",
+        "    ios::sync_with_stdio(false);",
+        "    cin.tie(nullptr);",
+        "    ",
+        "    return 0;",
+        "}",
+      }
 
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
 
-    vim.api.nvim_win_set_cursor(0, { 8, 2 })
+      vim.api.nvim_win_set_cursor(0, { 7, 4 })
+    end
   end,
 })
-
 
 
 vim.o.background = "dark"
