67d3ca3cf303b162d6af36c2d8b77ec3cce4a934
phantanlongzzz <phantanlong121102@gmail.com>
2026-06-23 21:04:10 +0700

    perf: toi uu hoa keymaps va sua loi trung phim F5

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,47 +1,50 @@
+-- ĐỊNH NGHĨA PHÍM TẮT TRONG NEOVIM
 local keymap = vim.keymap
+local opts = { silent = true }
 
+-- File Explorer (NvimTree)
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+-- Telescope (Tối ưu hóa: Giờ chỉ load khi bấm phím)
+keymap.set("n", "<leader>ff", function() require("telescope.builtin").find_files() end, {})
+keymap.set("n", "<leader>fg", function() require("telescope.builtin").live_grep() end, {})
+keymap.set("n", "<leader>fb", function() require("telescope.builtin").buffers() end, {})
 
-local builtin = require("telescope.builtin")
-keymap.set("n", "<leader>ff", builtin.find_files, {})
-keymap.set("n", "<leader>fg", builtin.live_grep, {})
-keymap.set("n", "<leader>fb", builtin.buffers, {})
+-- ToggleTerm Config (Tối ưu hóa: Đưa vào function để lazy load)
+local main_term = nil
+local function get_main_term()
+    if not main_term then
+        local Terminal = require("toggleterm.terminal").Terminal
+        main_term = Terminal:new({
+            direction = "horizontal",
+            hidden = true,
+        })
+    end
+    return main_term
+end
 
-
-local Terminal = require("toggleterm.terminal").Terminal
-
-local main_term = Terminal:new({
-    direction = "horizontal",
-    hidden = true,
-})
-
+-- F4: Bật/Tắt nhanh Terminal
 keymap.set("n", "<F4>", function()
-    main_term:toggle()
-end, { silent = true })
+    get_main_term():toggle()
+end, opts)
 
-
-
--- F5: Python
+-- F5: Lưu và Chạy file Python qua ToggleTerm
 keymap.set("n", "<F5>", function()
     vim.cmd("w")
-
     local file = vim.fn.expand("%:p")
+    local term = get_main_term()
+    term:open()
+    term:send("python \"" .. file .. "\"", true)
+end, opts)
 
-    main_term:open()
-    main_term:send("python \"" .. file .. "\"", true)
-end)
-
---F6 compile + run C++
+-- F6: Lưu, Biên dịch chuẩn CP (-std=c++23) và Chạy file C++ qua ToggleTerm
 keymap.set("n", "<F6>", function()
     vim.cmd("w")
-
     local file = vim.fn.expand("%:p")
     local exe = vim.fn.expand("%:r") .. ".exe"
-
-    main_term:open()
-
-    main_term:send(
+    local term = get_main_term()
+    term:open()
+    term:send(
         'g++ -std=c++23 -O2 -Wall -Wextra "'
             .. file
             .. '" -o "'
@@ -51,14 +54,13 @@ keymap.set("n", "<F6>", function()
             .. '"',
         true
     )
-end)
+end, opts)
 
+-- Huez Theme Manager
+keymap.set("n", "<leader>th", ":Huez<CR>", opts)
+keymap.set("n", "<leader>tf", ":HuezFavorites<CR>", opts)
 
-keymap.set("n", "<leader>th", ":Huez<CR>", { silent = true })
-keymap.set("n", "<leader>tf", ":HuezFavorites<CR>", { silent = true })
-
-local opts = { silent = true }
-
+-- Điều hướng thông minh giữa các Cửa sổ (Bao gồm cả Cửa sổ Terminal)
 keymap.set("n", "<C-h>", "<C-w>h", opts)
 keymap.set("n", "<C-j>", "<C-w>j", opts)
 keymap.set("n", "<C-k>", "<C-w>k", opts)
@@ -69,28 +71,24 @@ keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
 keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
 keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
 
-
--- Competitive Programming
+-- Competitive Programming (CompetiTest)
+-- F7: Chọn ngôn ngữ và Tải bài từ Competitive Companion
 keymap.set("n", "<F7>", function()
     vim.ui.select(
         { "cpp", "py" },
-        { prompt = "Language:" },
+        { prompt = "Language Extension:" },
         function(choice)
-            if not choice then
-                return
-            end
-
-            require("competitest").setup({
-                received_files_extension = choice,
-            })
-
+            if not choice then return end
+            require("competitest.config").config.received_files_extension = choice
             vim.cmd("CompetiTest receive problem")
         end
     )
 end, opts)
 
+-- F8: Chạy thử toàn bộ các Testcase đã tải
 keymap.set("n", "<F8>", function()
     vim.cmd("CompetiTest run")
 end, opts)
 
+-- Tự động nhảy vào thư mục làm bài Codeforces khi mở Neovim
 vim.cmd([[cd E:/Computer Science/Codeforces]])
