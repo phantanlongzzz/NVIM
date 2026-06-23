91c3838002f5f04d838db6121987d1cae28b3749
phantanlongzzz <phantanlong121102@gmail.com>
2026-06-23 18:00:44 +0700

    github theme

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -3,6 +3,6 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("cursor-dark")
+    --vim.cmd.colorscheme("cursor-dark")
   end,
 }
---
+++ lua/plugins/github_theme.lua
@@ -0,0 +1,19 @@
+return {
+  "projekt0n/github-nvim-theme",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("github-theme").setup({
+      options = {
+        transparent = false,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    })
+
+    vim.cmd('colorscheme github_dark_default')
+  end,
+}
